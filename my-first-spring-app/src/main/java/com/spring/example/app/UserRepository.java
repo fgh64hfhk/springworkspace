02b37a59b5a3953caed1720a7d1253256b608606
fgh64hfhk <bear200806@gmail.com>
2024-05-21 11:32:48 +0800

    finish example on Accessing data with MySQL

---
+++ my-first-spring-app/src/main/java/com/spring/example/app/UserRepository.java
@@ -0,0 +1,7 @@
+package com.spring.example.app;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Integer>{
+
+}
---
+++ my-first-spring-app/src/test/java/com/spring/example/app/MyFirstSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.example.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
